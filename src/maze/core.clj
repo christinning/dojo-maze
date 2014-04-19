07ee4fcf247672b9aaae1b94371235e52403b8a2
Chris Tinning <chris.tinning@gmail.com>
2014-04-19 16:23:24 +0100

    Tidied up to return only one path in direction format

--- src/maze/core.clj
+++ src/maze/core.clj
@@ -1,88 +1,79 @@
 (ns maze.core
   (require [clojure.string :refer [split]]))
 
-(def sample
-  (str "#I###\n"
-       "#...O\n"
-       "#..##\n"
-       "#####\n"))
-
-(declare to-maze)
-(def sample-maze (to-maze sample))
-
-(defn path
-  [maze]
-  [:S :E :E :E])
-
-(defn mv
-  [[dx dy]]
-  (fn [[x y]] [(+ dx x) (+ dy y)]))
+(def exit-square \O)
+(def start-square \I)
+(def non-wall \.)
+(def wall \#)
+(def visited-square \V)
 
 (def moves
   {[0 -1] :N
-   [1 0] :E
-   [0 1] :S
-   [-1 0] :W })
-
-
-(defn apply-move
-  [coord move]
-  (vec (map + coord move)))
-
-
-
-(defn neighbours
-  [coord]
-  (map (partial apply-move coord)
-       (keys moves)))
-
-
-
-
-(map + [1 1] [2 2])
-
-(def target \O)
-(defn valid-location
-  [maze coord]
-  (contains? #{\. \I \O} (get-in maze coord)))
-
-(defn walk
-  [maze path]
-  (let [loc (last path)
-        next-maze (assoc-in maze loc \#)]
-    ;;(println path loc )
-    ;;(println (get-in maze loc))
-    (if (= (get-in maze loc) target)
-      path
-      (map
-       (fn [next] (walk next-maze (conj path next)))
-       (filter (partial valid-location maze)
-              (neighbours loc))))))
-
-
-
+   [1  0] :E
+   [0  1] :S
+   [-1 0] :W})
 
 (defn to-maze
   "Constructs a maze data structure from a \\\n deliminated String.
   Returns a vector of columns to allow easy (get-in maze [x y]) access"
-  [^String s]
+  [s]
   (vec ( apply
          map vector
          (split s #"\n"))))
-
-
 
 (defn start-location
   [maze]
   (loop
     [x 0]
     (let
-      [y (.indexOf (nth maze x) \I)]
+      [y (.indexOf (nth maze x) start-square)]
       (if (not= y  -1)
         [x y]
         (recur (inc x))))))
-(start-location sample-maze)
 
-(walk sample-maze [(start-location sample-maze)])
+(defn path-to-directions
+  "Converts a path of locations into directions. [[1 1] [1 2] [2 2]] becomes [:S :E]"
+  [path]
+  (map moves
+       (map
+        #(vec (apply map - (reverse %)) )
+        (partition 2 1 path))))
+
+(defn neighbours
+  [loc]
+  (map #(vec (map + loc %))
+       (keys moves)))
+
+(defn valid-location
+  [maze loc]
+  (let [current-loc (get-in maze loc)]
+    (or (= current-loc exit-square)
+        (= current-loc non-wall)
+        (= current-loc start-square))))
+
+(defn valid-neighbours
+  [maze loc]
+  (filter (partial valid-location maze)
+              (neighbours loc)))
+
+;;TODO: update to do breadth first and only return first path
+(defn walk
+  [maze path]
+  (let [loc (last path)]
+    (if (= (get-in maze loc) exit-square)
+      [path]
+      (mapcat
+        (fn [next-loc]
+          (walk (assoc-in maze loc visited-square)
+                (conj path next-loc)))
+        (valid-neighbours maze loc)))))
+
+(defn solve-maze
+  [str-maze]
+  (let [maze (to-maze str-maze)
+        start (start-location maze)]
+    (path-to-directions(first (sort-by count (walk maze [start]))))))
+
+
 
 
--- test/maze/core_test.clj
+++ test/maze/core_test.clj
@@ -3,13 +3,28 @@
             [maze.core :refer :all]))
 
 
-(def sample
-  (str "#I###\n"
-       "#...O\n"
-       "#..##\n"
+(def simple
+  (str "###I#\n"
+       "O...#\n"
+       "##..#\n"
        "#####\n"))
+
+(def complex
+  (str "#I#####\n"
+       "#...#.O\n"
+       "#.#.#.#\n"
+       "#.#.#..\n"
+       "..#.##.\n"
+       ".##..#.\n"
+       "...#.#.\n"
+       "##.....\n"
+       "###.###\n"
+       ))
 
 (deftest can-get-path
   (testing "Simple maze"
-    (is (= (path sample)
-           [:S :E :E :E]))))
+    (is (= (solve-maze simple)
+           [:S :W :W :W])))
+  (testing "Complex maze"
+    (is (= (solve-maze complex)
+           [:S :E :E :S :S :S :S :E :S :S :E :E :N :N :N :N :W :N :N :E]))))
