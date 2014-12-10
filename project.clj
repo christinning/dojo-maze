1a2b7a8d6b80beb9c0bb88ea7062a509f4b1f122
Chris Tinning <chris.tinning@gmail.com>
2014-12-10 19:02:54 +0000

    Added placeholder for using test.check to generate mazes for testing

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "London Clojure Easter dojo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.1"]]}})
